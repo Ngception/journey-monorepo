c07c61920e8820635ee820fcef2331ac09c34b8a
Patrick Ng <patrick.ng@acquia.com>
2022-05-19 17:00:07 -0400

    JRNY-18 | Research style libraries (#2)

--- apps/journey-ui/src/app/app.tsx
+++ apps/journey-ui/src/app/app.tsx
@@ -1,10 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
+import 'semantic-ui-css/semantic.min.css';
 import NxWelcome from './nx-welcome';
+import { Dimmer, Loader, Image, Segment } from 'semantic-ui-react';
 
 export function App() {
   return (
     <>
+      <Loader active={true} />
       <NxWelcome title="journey-ui" />
       <div />
     </>
