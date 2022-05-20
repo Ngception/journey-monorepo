42a1831fd62bca6f152908aa55ae0e9a3e075b62
Patrick Ng <patrick.ng@acquia.com>
2022-05-19 23:18:56 -0400

    JRNY-36 | Replace Semantic-UI React (#3)

--- apps/journey-ui/src/app/app.tsx
+++ apps/journey-ui/src/app/app.tsx
@@ -1,13 +1,21 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-import 'semantic-ui-css/semantic.min.css';
+import 'bulma/css/bulma.min.css';
 import NxWelcome from './nx-welcome';
-import { Dimmer, Loader, Image, Segment } from 'semantic-ui-react';
 
 export function App() {
   return (
     <>
-      <Loader active={true} />
+      <div className="buttons">
+        <button className="button is-primary">Primary</button>
+        <button className="button is-link">Link</button>
+      </div>
+
+      <div className="buttons">
+        <button className="button is-info">Info</button>
+        <button className="button is-success">Success</button>
+        <button className="button is-warning">Warning</button>
+        <button className="button is-danger">Danger</button>
+      </div>
       <NxWelcome title="journey-ui" />
       <div />
     </>
