b220f10999cd7d6f6c915d5c5400338450647117
Patrick Ng <patrick.ng@acquia.com>
2022-05-29 13:00:48 -0400

    JRNY-42 | Scaffold the accounts API (#19)

---
+++ apps/accounts-api/src/app/app.controller.spec.ts
@@ -0,0 +1,33 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+  let app: TestingModule;
+
+  beforeAll(async () => {
+    app = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+  });
+
+  describe('getData', () => {
+    it('should return "Welcome to accounts-api!"', () => {
+      const appController = app.get<AppController>(AppController);
+      expect(appController.getData()).toEqual({
+        message: 'Welcome to accounts-api!',
+      });
+    });
+  });
+
+  describe('getStatus', () => {
+    it('should return statys', () => {
+      const appController = app.get<AppController>(AppController);
+      expect(appController.getStatus()).toEqual({
+        message: 'OK',
+      });
+    });
+  });
+});
