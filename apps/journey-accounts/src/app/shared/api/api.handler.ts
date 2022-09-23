1db25af8db5d795b8010ea3a07efcae4f029e4c2
Ngception <113420241+Ngception@users.noreply.github.com>
2022-09-22 20:13:16 -0400

    feat: add handler

--- apps/journey-accounts/src/app/shared/api/api.handler.ts
+++ apps/journey-accounts/src/app/shared/api/api.handler.ts
@@ -2,7 +2,8 @@ import axios, { AxiosError, AxiosResponse } from 'axios';
 import { HttpException } from '@nestjs/common';
 import { ICreateUser, IDeleteUser, IUpdateUser } from '@journey-monorepo/util';
 
-const BASE_URL = process.env['NX_AAPI_BASE_URL'];
+const AAPI_BASE_URL = process.env['NX_AAPI_BASE_URL'];
+const JAPI_BASE_URL = process.env['NX_JAPI_BASE_URL'];
 
 export const handleError = (error: AxiosError<HttpException>) => {
   return error?.response?.data;
@@ -18,7 +19,7 @@ export const createUser = async (createUserData: ICreateUser) => {
       email: string;
       created_at: Date;
     };
-  }> = await axios.post(`${BASE_URL}/users`, createUserData, {
+  }> = await axios.post(`${AAPI_BASE_URL}/users`, createUserData, {
     withCredentials: true,
   });
 
@@ -27,7 +28,7 @@ export const createUser = async (createUserData: ICreateUser) => {
 
 export const updateUser = async (updateUserData: IUpdateUser) => {
   const { data }: AxiosResponse<number> = await axios.patch(
-    `${BASE_URL}/users/${updateUserData.user_id}`,
+    `${AAPI_BASE_URL}/users/${updateUserData.user_id}`,
     {
       password: updateUserData.password,
     },
@@ -41,8 +42,20 @@ export const updateUser = async (updateUserData: IUpdateUser) => {
 
 export const deleteUser = async (deleteUserData: IDeleteUser) => {
   const { data }: AxiosResponse<number> = await axios.delete(
-    `${BASE_URL}/users/${deleteUserData.user_id}`,
+    `${AAPI_BASE_URL}/users/${deleteUserData.user_id}`,
     {
+      withCredentials: true,
+    }
+  );
+
+  return data;
+};
+
+export const deleteTasksByUserId = async (userId: string) => {
+  const { data }: AxiosResponse<number> = await axios.delete(
+    `${JAPI_BASE_URL}/tasks`,
+    {
+      data: { user_id: userId },
       withCredentials: true,
     }
   );
