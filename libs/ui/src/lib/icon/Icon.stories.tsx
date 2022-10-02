e7702f8fec53142b51bd6e3a610be7a54a5c496b
Ngception <113420241+Ngception@users.noreply.github.com>
2022-10-02 18:38:38 -0400

    feat: add icon story

---
+++ libs/ui/src/lib/icon/Icon.stories.tsx
@@ -0,0 +1,27 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Icon } from './Icon';
+
+export default {
+  title: 'Icon',
+  component: Icon,
+  argTypes: {
+    type: {
+      options: ['solid', 'regular'],
+      control: 'select',
+    },
+    name: {
+      control: 'text',
+    },
+  },
+} as ComponentMeta<typeof Icon>;
+
+export const Default: ComponentStory<typeof Icon> = (args) => (
+  <span className="is-size-1">
+    <Icon {...args} />
+  </span>
+);
+
+Default.args = {
+  type: 'solid',
+  name: 'check-double',
+};
