de19317c8d83de2b2c9dcb648eb5045c10877703
Patrick Ng <113420241+Ngception@users.noreply.github.com>
2022-10-03 17:34:02 -0400

    Revert "JRNY-166 | Add storybook (#6)" (#9)

--- libs/ui/src/lib/loader/Loader.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { Loader } from './Loader';
-
-export default {
-  title: 'Loader',
-  component: Loader,
-} as ComponentMeta<typeof Loader>;
-
-export const Default: ComponentStory<typeof Loader> = (args) => <Loader />;
